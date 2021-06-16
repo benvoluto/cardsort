6eb3dab2a06a0b449de596efc4e9eca1c0256306
Ben Clemens <benc@philo.com>
2021-06-15 21:36:14 -0700

    touch drag, second try

--- src/card.js
+++ src/card.js
@@ -2,17 +2,17 @@ import React, { useRef } from 'react';
 import { useDrag } from 'react-dnd';
 import { usePreview } from 'react-dnd-preview';
  
-const MyPreview = ({card}) => {
+const MyPreview = ({text}) => {
   // disabling since the dnd lib needs an item in this scope
   // eslint-disable-next-line no-unused-vars
   const {display, item, style} = usePreview();
   if (!display) {
     return null;
   }
-  return <div className="item-list__item" style={style}>{card}</div>;
+  return <div className="item-list__item" style={style}><div className="card">{text}</div></div>;
 };
 
-const Card = ({ id, children }) => {
+const Card = ({ id, text, children }) => {
   const ref = useRef(null);
   const [{ isDragging }, drag] = useDrag({
     item: { type: 'card', id },
@@ -25,7 +25,7 @@ const Card = ({ id, children }) => {
   return (
     <div ref={ref} style={{ opacity }}>
       {children}
-      <MyPreview card={children} />
+      <MyPreview text={text} />
     </div>
   );
 };
